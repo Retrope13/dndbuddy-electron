6fac7bd611d008de0653e6c43ca3d2fa6070c00f
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2024-05-06 09:18:50 -0600

    get WIP container working

--- src/components/CharacterContainer.js
+++ src/components/CharacterContainer.js
@@ -8,6 +8,15 @@ function CharacterContainer() {
         <input
           type="text"
           class="form-control"
+          placeholder="Player Name"
+          aria-label="Player Name"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+      <div class="input-group-a1">
+        <input
+          type="text"
+          class="form-control"
           placeholder="Character Name"
           aria-label="character Name"
           aria-describedby="basic-addon1"
@@ -26,8 +35,26 @@ function CharacterContainer() {
         <input
           type="text"
           class="form-control"
-          placeholder="Player Name"
-          aria-label="Player Name"
+          placeholder="Character Race"
+          aria-label="Character Race"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+      <div class="input-group-a1">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Alignment"
+          aria-label="Alignment"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+      <div class="input-group-a1">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="EXP"
+          aria-label="Class and level"
           aria-describedby="basic-addon1"
         />
       </div>
