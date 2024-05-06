6fb9520a7d2727faebc824445962b817201b0cbe
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2024-05-06 08:46:51 -0600

    add characterContainer to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CustomTabs from "./components/CustomTabs"; // Import your custom component
+import CharacterContainer from "./components/CharacterContainer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { PlayerCharacter } from "./Classes/PlayerCharacter";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <h1>Example heading</h1>
+      <CharacterContainer />
       <CustomTabs />
     </div>
   );
