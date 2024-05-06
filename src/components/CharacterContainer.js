994e10ff5bdf52665cc36a2b82ecc3b3481a8910
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2024-05-06 08:46:17 -0600

    Create a flex div that contains character information

---
+++ src/components/CharacterContainer.js
@@ -0,0 +1,38 @@
+// FlexContainer.js
+import React, { useState } from "react";
+
+function CharacterContainer() {
+  return (
+    <div id="flexContainer">
+      <div class="input-group-a1">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Character Name"
+          aria-label="character Name"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+      <div class="input-group-a1">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Class & level"
+          aria-label="Class and level"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+      <div class="input-group-a1">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Player Name"
+          aria-label="Player Name"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default CharacterContainer;
