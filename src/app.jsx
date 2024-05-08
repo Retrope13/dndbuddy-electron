c2e690fc50096499666ae77f0a060b85aec7596c
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2024-05-08 16:27:06 -0600

    Oops I forgot to make commits. I added the readWeapons function after a lot of confusion. Next on this is to make sure that each weapon is on its own line. It's harder than it sounds rn

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import ReactDOM from "react-dom";
 import CustomTabs from "./components/CustomTabs"; // Import your custom component
 import CharacterContainer from "./components/CharacterContainer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [WeaponStoreList, setWeaponStoreList] = useState([]);
+  let WeaponStoreJSON = require("./itemFiles/Weapons.json");
+  //this will be for later when I import characters. I'm hoping I can use all of the same functions for the store and character
+  let CharJSON;
+  function readWeapons(WeaponFile) {
+    WeaponFile.forEach((element) =>
+      WeaponStoreList.push(
+        <div className="WeaponItem" key={element.name}>
+          {element.name} {element.damage} {element.damageType} {element.price}
+          <button onClick={() => handleButtonClick(element)}>Buy</button>
+        </div>
+      )
+    );
+    console.log(WeaponStoreList);
+  }
+
+  function readArmor() {
+    //var ArmorJSON = require("./itemFiles/Armor.json");
+  }
+
+  function readSpells() {
+    //var SpellsJSON = require("./itemFiles/Spells.json");
+  }
+
+  //T
+  readWeapons(WeaponStoreJSON);
   return (
     <div id="wrapperDiv">
       <h1>Example heading</h1>
       <CharacterContainer />
       <br></br>
-      <CustomTabs weapons={"mace"} armor={"leather"} spells={"magic missle"} />
+      <label>Inventory</label>
+      <label>Store</label>
+      <div id="CustomTabsDiv">
+        <CustomTabs
+          id="invTabs"
+          weapons={"mace"}
+          armor={"leather"}
+          spells={"magic missle"}
+        />
+        <CustomTabs
+          id="storeTabs"
+          weapons={WeaponStoreList}
+          armor={"leather"}
+          spells={"magic missle"}
+        />
+      </div>
     </div>
   );
 }
